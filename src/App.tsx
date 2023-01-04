9f82e5acf286daf1794f7f04f5a97afb72cf08ae
Lelouching <engenheirogamer123@gmail.com>
2023-01-04 18:22:02 -0300

    removed some props unecessary and now are optional

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Button } from "./components/Button";
 import { Global } from "./styles/global";
 
 function App() {
   return (
     <>
       <Global/>
-      <Button styleButton="style2">
-        Teste
-      </Button>
     </>
   );
 }
