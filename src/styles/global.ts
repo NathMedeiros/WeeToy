59a8a10aeae69f510ad8570a25d776704d39a49b
Lelouching <engenheirogamer123@gmail.com>
2023-01-04 13:49:58 -0300

    font-family added to button and inter added to body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const Global = createGlobalStyle`
 
     @import url('https://fonts.googleapis.com/css2?family=Seymour+One&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
     *{
         padding: 0;
@@ -15,6 +16,12 @@ export const Global = createGlobalStyle`
     :root{
         --color-grey-0: #f4f4f4;
         --font-button: font-family: 'Seymour One', sans-serif;
+    }
+
+    body{
+        font-family: 'Inter', sans-serif;
+        width: 100%;
+        min-height: 100vh;
     }
 
     button{
