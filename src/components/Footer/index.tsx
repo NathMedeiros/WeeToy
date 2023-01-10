40ed3df9b32bddcdae714e7d9253b15077c0dbff
ricardocza <ricardovcza@gmail.com>
2023-01-10 14:54:27 -0300

    feat: add footer structure

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,5 @@
+import { StyledFooter } from "./style";
+
+export function Footer() {
+  return <StyledFooter></StyledFooter>;
+}
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,7 @@
+import { styled } from "goober";
+
+export const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  background-color: red;
+`;
