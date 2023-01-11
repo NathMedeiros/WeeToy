5f92a55532fb52ca0318a642911e94f45ab9b1d0
Nath Medeiros <nathalia_lalvc@hotmail.com>
2023-01-11 14:01:00 -0300

    attModalDescription

--- src/context/CardModalContext.tsx
+++ src/context/CardModalContext.tsx
@@ -3,8 +3,6 @@ import { createContext, useState } from "react";
 import { iModalProps } from "./LoginContext";
 
 export interface iCardContext {
-  // openCard: () => void;
-  // closeCard: () => void;
   cardOpen: number | null;
   setCardOpen: React.Dispatch<React.SetStateAction<number | null>>;
   modalDescription: (id: number) => void;
@@ -15,16 +13,7 @@ export const CardModalContext = createContext({} as iCardContext);
 export function CardProvider({ children }: iModalProps) {
   const [cardOpen, setCardOpen] = useState<number | null>(null);
 
-  // function openCard() {
-  //   setCardOpen(true);
-  // }
-
-  // function closeCard() {
-  //   setCardOpen(false);
-  // }
-
   const modalDescription = (id: number) => {
-    // abrir e fechar modal edit donation
     if (!cardOpen) {
       setCardOpen(id);
     } else {
